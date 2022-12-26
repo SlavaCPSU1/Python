ccb404042c2fcc124571f29ba70be21c64b55db4
Ben <s.i.ares@mail.ru>
2022-12-26 21:40:30 +0300

    add dz3.1

--- DZ3/dz3.py
+++ DZ3/dz3.py
@@ -50,7 +50,7 @@
 # x = int(input("Введите число X: "))
 
 
-# def find_num(count):
+# def number(count):
 
 #     for i in range(0, len(array)):
 #         if array[i] == x - count:
@@ -58,7 +58,57 @@
 #         elif array[i] == x + count:
 #             return array[i]
 
-#     return find_num(count + 1)
+#     return number(count+1)
 
 
-# print(find_num(0))
+# print(number(0))
+
+# Задача 20:
+# В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+# D, G – 2 очка;
+# B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка;
+# K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
+
+# А русские буквы оцениваются так:
+# А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
+
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
+
+# Ввод: ноутбук
+# Вывод: 12
+print('Напишите программу, которая вычисляет стоимость введенного пользователем слова.'
+ 'Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.'
+ 'Write a program that calculates the cost of a word entered by the user.'
+ 'We will assume that only one word is given as input, which contains either only English or only Russian letters.')
+
+dictionary = {   1 :'А, В, Е, И, Н, О, Р, С, Т, A, E, I, O, U, L, N, S, T, R' ,
+                 2 :'D, G, Д, К, Л, М, П, У'  ,
+                 3 : 'B, C, M, P, Б, Г, Ё, Ь, Я' ,
+                 4 : 'F, H, V, W, Y, Й, Ы' ,
+                 5 : 'K, Ж, З, Х, Ц, Ч' ,
+                 8 : 'J, X, Ш, Э, Ю' ,
+                 10: 'Q, Z, Ф, Щ, Ъ'
+}
+
+slovo = input('Введите слово на русском или английском языке\Enter a word in Russian or English: ')
+slovo = slovo.upper()
+
+res = 0
+for i in slovo:
+    if str.isalpha():
+        res += dictionary
+        print('Стоимость слова\Word cost: ')
+    else:
+        print('Некорректный ввод/Incorrect input')
