8113d852478f50bdcb9374cd0cd09cee2b3b5c15
Ben <s.i.ares@mail.ru>
2022-12-27 22:14:25 +0300

    add dz3.2

--- DZ3/dz3.py
+++ DZ3/dz3.py
@@ -88,27 +88,48 @@
 
 # Ввод: ноутбук
 # Вывод: 12
-print('Напишите программу, которая вычисляет стоимость введенного пользователем слова.'
- 'Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.'
- 'Write a program that calculates the cost of a word entered by the user.'
- 'We will assume that only one word is given as input, which contains either only English or only Russian letters.')
 
-dictionary = {   1 :'А, В, Е, И, Н, О, Р, С, Т, A, E, I, O, U, L, N, S, T, R' ,
-                 2 :'D, G, Д, К, Л, М, П, У'  ,
-                 3 : 'B, C, M, P, Б, Г, Ё, Ь, Я' ,
-                 4 : 'F, H, V, W, Y, Й, Ы' ,
-                 5 : 'K, Ж, З, Х, Ц, Ч' ,
-                 8 : 'J, X, Ш, Э, Ю' ,
-                 10: 'Q, Z, Ф, Щ, Ъ'
+dictionary = {   
+1 :'А, В, Е, И, Н, О, Р, С, Т, A, E, I, O, U, L, N, S, T, R' ,
+2 :'D, G, Д, К, Л, М, П, У'  ,
+3 : 'B, C, M, P, Б, Г, Ё, Ь, Я' ,
+4 : 'F, H, V, W, Y, Й, Ы' ,
+5 : 'K, Ж, З, Х, Ц, Ч' ,
+8 : 'J, X, Ш, Э, Ю' ,
+10: 'Q, Z, Ф, Щ, Ъ'
 }
 
 slovo = input('Введите слово на русском или английском языке\Enter a word in Russian or English: ')
-slovo = slovo.upper()
+slovo = slovo.lower()
 
 res = 0
 for i in slovo:
-    if str.isalpha():
-        res += dictionary
-        print('Стоимость слова\Word cost: ')
-    else:
-        print('Некорректный ввод/Incorrect input')
+    res += dictionary[i]
+    print('Стоимость слова\Word cost:', res)
+    
+
+# dictionary = {
+# 'a':1, 'e':1, 'i':1, 'l':1, 'n':1, 'o':1, 'r':1, 's':1, 't':1, 'u':1,
+# 'd':2, 'g':2,
+# 'b':3, 'c':3, 'm':3, 'p':3,
+# 'f':4, 'h':4, 'v':4, 'w':4, 'y':4,
+# 'k':5,
+# 'j':8, 'x':8,
+# 'q':10, 'z':10,
+# 'а':1, 'в':1, 'е':1, 'и':1, 'н':1, 'о':1, 'р':1, 'с':1, 'т':1,
+# 'д':2, 'к':2, 'л':2, 'м':2, 'п':2, 'у':2,
+# 'б':3, 'г':3, 'ё':3, 'ь':3, 'я':3,
+# 'й':4, 'ы':4,
+# 'ж':5, 'з':5, 'х':5, 'ц':5, 'ч':5,
+# 'ш':8, 'э':8, 'ю':8,
+# 'ф':10, 'щ':10, 'ъ':10
+# }
+
+# word = input('Введите слово -> ')
+# word = word.lower()
+
+# result = 0
+# for i in word:
+#     result += dictionary[i]
+
+# print('Стоимость введенного вами слова:', result)
